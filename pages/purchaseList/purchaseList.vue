42ef80a7b89f84976f1294b95c684e01045032b0
java110 <928255095@qq.com>
2020-05-07 09:03:45 +0800

    合并采购相关功能

---
+++ pages/purchaseList/purchaseList.vue
@@ -0,0 +1,96 @@
+<template>
+	<view>
+		<view>
+			<view class="padding">商品信息</view>
+			<view class="cu-list menu ">
+				<view class="cu-item" v-for="(item,sub) in puchaseAll" :key="sub" @tap="_doChangeFloor(item)">
+					<view class="content padding-tb-sm">
+						<view>
+							<text class="lg  cuIcon-list text-blue margin-right-xs"></text> 名称:{{item.resName}}</view>
+						<view class="text-gray text-sm">
+							<text class="lg text-gray cuIcon-edit margin-right-xs"></text> 库存:{{item.stock}}</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				puchaseAll:'',
+				floorNum:'',
+				communityId:'',
+				floors:[]
+			}
+		},
+		
+		onLoad(options) {
+			this.communityId = options.communityId;
+			this._loadPurchase();
+		},
+		methods: {
+			
+			//加载楼栋信息
+			_loadPurchase:function(){
+				let _communityId = this.communityId;
+				
+				let _that = this;
+				let _userInfo = this.java110Context.getUserInfo();
+				let storeId = _userInfo.storeId;
+				let _objData = {
+					page: 1,
+					row: 15,
+					storeId: storeId,
+					communityId:_that.java110Context.getCurrentCommunity().communityId
+				};
+				this.java110Context.request({
+					url: _that.java110Constant.url.listResourceStores,
+					header: _that.java110Context.getHeaders(),
+					method: "GET",
+					data: _objData, //动态数据
+					success: function(res) {
+						console.log("请求返回信息：", res);
+						if (res.statusCode != 200) {
+							uni.showToast({
+								icon:'none',
+								title:res.data
+							});
+							return;
+						}
+						_that.puchaseAll = res.data.resourceStores;
+						console.log(_that.puchaseAll)
+					},
+					fail: function(e) {
+						wx.showToast({
+							title: "服务器异常了",
+							icon: 'none',
+							duration: 2000
+						});
+					}
+				});
+				
+			},
+			_doChangeFloor:function(_floor){
+				// let pages = getCurrentPages();
+				// var prevPage = pages[pages.length - 2];
+				// prevPage.floorId = _floor.floorId;
+				// prevPage.floorNum = _floor.floorNum;
+				console.log(_floor)
+				this.java110Context.setParam("floorInfo",_floor);
+			
+				uni.navigateBack({
+					delta:1
+				});
+			}
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/purchaseReview/purchaseReview.vue
@@ -0,0 +1,68 @@
+<template>
+	<view>
+		<scroll-view scroll-x class="bg-white nav">
+			<view class="flex text-center">
+				<view class="cu-item flex-sub" :class="active==0?'text-green cur':''" @tap="tabSelect(0)">
+					待处理
+				</view>
+				<view class="cu-item flex-sub" :class="active==1?'text-green cur':''" @tap="tabSelect(1)">
+					接单中
+				</view>
+				<view class="cu-item flex-sub" :class="active==2?'text-green cur':''" @tap="tabSelect(2)">
+					完成
+				</view>
+			</view>
+		</scroll-view>
+		<!---->
+		<view v-if="active == 0" class="ppfl_c" >
+			
+		</view>
+
+		<view v-if="active == 1" class="ppfl_c" >
+			
+		</view>
+
+		<view v-if="active == 2" class="ppfl_c" >
+			
+		</view>
+
+
+	</view>
+</template>
+
+<script>
+	const context = require('../../context/Java110Context.js');
+	const factory = context.factory;;
+	const constant = context.constant;
+	export default {
+		data() {
+			return {
+				active: 0,
+			}
+		},
+		/**
+		 * 生命周期函数--监听页面加载
+		 */
+		onLoad: function(options) {
+			
+		},
+		methods: {
+			onChange: function(e) {
+				console.log(e);
+
+				this._loadRepair(e.detail.index);
+			},
+			tabSelect: function(_active) {
+				this.active = _active;
+				this._loadRepair(_active);
+			}
+		}
+	}
+</script>
+
+<style>
+	.solid-bottom::after {
+		border-bottom: 2upx solid rgba(0, 0, 0, 0.1);
+	}
+
+</style>

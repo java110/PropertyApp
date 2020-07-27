2d120b02069156bb02d395880711e6dd1e24da44
java110 <928255095@qq.com>
2020-07-27 17:23:07 +0800

    加入没有数据展示

--- pages/repairDispatchFinish/repairDispatchFinish.vue
+++ pages/repairDispatchFinish/repairDispatchFinish.vue
@@ -1,61 +1,70 @@
 <template>
 	<view>
-		
-		<view v-for="(item,index) in myOrders" :key="index" class="bg-white margin-top margin-right-xs radius margin-left-xs padding">
-			<view class="flex padding-bottom-xs solid-bottom justify-between">
-				<view>{{item.repairId}}</view>
-				<view class="text-gray">{{item.tel}}</view>
+		<view v-if="noData==false">
+			<view v-for="(item,index) in myOrders" :key="index" class="bg-white margin-top margin-right-xs radius margin-left-xs padding">
+				<view class="flex padding-bottom-xs solid-bottom justify-between">
+					<view>{{item.repairId}}</view>
+					<view class="text-gray">{{item.tel}}</view>
+				</view>
+				<view class="flex margin-top justify-between">
+					<view class="text-gray">报修类型</view>
+					<view class="text-gray">{{item.repairTypeName}}</view>
+				</view>
+				<view class="flex margin-top-xs justify-between">
+					<view class="text-gray">报修人</view>
+					<view class="text-gray">{{item.repairName}}</view>
+				</view>
+				<view class="flex margin-top-xs justify-between">
+					<view class="text-gray">位置</view>
+					<view class="text-gray">{{item.repairObjName}}</view>
+				</view>
+				<view class="flex margin-top-xs justify-between">
+					<view class="text-gray">预约时间</view>
+					<view class="text-gray">{{item.appointmentTime }}</view>
+				</view>
+				<view class="flex margin-top-xs justify-between">
+					<view class="text-gray">状态</view>
+					<view class="text-gray">{{item.stateName}}</view>
+				</view>
+				<view class="flex margin-top-xs justify-between">
+					<view class="text-gray">报修内容</view>
+					<view class="text-gray">{{item.context}}</view>
+				</view>
+				<view class="solid-top flex justify-end margin-top padding-top-sm ">
+					<button class="cu-btn sm line-gray" @click="repairDetail(item)">详情</button>
+				</view>
 			</view>
-			<view class="flex margin-top justify-between">
-				<view class="text-gray">报修类型</view>
-				<view class="text-gray">{{item.repairTypeName}}</view>
-			</view>
-			<view class="flex margin-top-xs justify-between">
-				<view class="text-gray">报修人</view>
-				<view class="text-gray">{{item.repairName}}</view>
-			</view>
-			<view class="flex margin-top-xs justify-between">
-				<view class="text-gray">位置</view>
-				<view class="text-gray">{{item.repairObjName}}</view>
-			</view>
-			<view class="flex margin-top-xs justify-between">
-				<view class="text-gray">预约时间</view>
-				<view class="text-gray">{{item.appointmentTime }}</view>
-			</view>
-			<view class="flex margin-top-xs justify-between">
-				<view class="text-gray">状态</view>
-				<view class="text-gray">{{item.stateName}}</view>
-			</view>
-			<view class="flex margin-top-xs justify-between">
-				<view class="text-gray">报修内容</view>
-				<view class="text-gray">{{item.context}}</view>
-			</view>
-			<view class="solid-top flex justify-end margin-top padding-top-sm ">
-				<button class="cu-btn sm line-gray" @click="repairDetail(item)">详情</button>
-			</view>
+		</view>
+		<view v-else>
+			<no-data-page></no-data-page>
 		</view>
 	</view>
 </template>
 
 <script>
+	import noDataPage from '@/components/no-data-page/no-data-page.vue'
 	export default {
 		data() {
 			return {
-				orderImg:this.java110Constant.url.baseUrl + 'img/order.png',
-				myOrders:[],
-				orders:[],
-				storeId:''
+				orderImg: this.java110Constant.url.baseUrl + 'img/order.png',
+				myOrders: [],
+				orders: [],
+				storeId: '',
+				noData: false
 			}
 		},
-		onLoad(){
-			
+		components: {
+			noDataPage
+		},
+		onLoad() {
+
 			let _userInfo = this.java110Context.getUserInfo();
 			let _storeId = _userInfo.storeId;
 			this.storeId = _storeId;
 			this._loadMyModify();
 		},
 		methods: {
-			_loadMyModify:function(){
+			_loadMyModify: function() {
 				//
 				let _that = this;
 				let _userInfo = this.java110Context.getUserInfo();
@@ -63,10 +72,10 @@
 				let _objData = {
 					page: 1,
 					row: 15,
-					userId:_userInfo.userId,
-					communityId:_that.java110Context.getCurrentCommunity().communityId
+					userId: _userInfo.userId,
+					communityId: _that.java110Context.getCurrentCommunity().communityId
 				};
-				
+
 				this.java110Context.request({
 					url: _that.java110Constant.url.listStaffFinishRepairs,
 					header: _that.java110Context.getHeaders(),
@@ -84,7 +93,10 @@
 						}
 						let _data = _json.data;
 						_that.myOrders = _data;
-						
+						if (_that.myOrders.length < 1) {
+							_that.noData = true;
+							return;
+						}
 						_that.myOrders.forEach(function(item) {
 							let dateStr = item.appointmentTime;
 							console.log(dateStr);
@@ -102,13 +114,13 @@
 					}
 				});
 			},
-			repairDetail:function(_item){
-				console.log('_item',_item);
+			repairDetail: function(_item) {
+				console.log('_item', _item);
 				//wx.setStorageSync("_toModifyComplaint_"+_item.complaintId, _item);
 				uni.navigateTo({
-					url:"/pages/repairDetail/repairDetail?repairId=" + _item.repairId+'&storeId='+this.storeId
-					
-				});	
+					url: "/pages/repairDetail/repairDetail?repairId=" + _item.repairId + '&storeId=' + this.storeId
+
+				});
 			}
 		}
 	}

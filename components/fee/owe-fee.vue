ca59e51665156b91094138d7243ca573aff92fdd
Your Name <you@example.com>
2023-09-15 10:29:55 +0800

    优化动态单价问题

--- components/fee/owe-fee.vue
+++ components/fee/owe-fee.vue
@@ -42,8 +42,8 @@
 						<view class="sub-info-item text-gray text-sm" v-if="item.payerObjName">
 							<text class="margin-right-xs">房号:{{item.payerObjName}}</text>
 						</view>
-						<view class="sub-info-item text-gray text-sm">
-							<text class="margin-right-xs">单价:{{item.squarePrice}}</text>
+						<view class="sub-info-item text-gray text-sm" >
+							<text class="margin-right-xs">单价:{{item.mwPrice && item.mwPrice>0?item.mwPrice:item.squarePrice}}</text>
 						</view>
 					</view>
 				</view>
